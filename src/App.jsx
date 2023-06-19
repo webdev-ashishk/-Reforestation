5c23cee79b11daef8e4859d6f95fe939d04586fd
webdev-ashishk <webdev.ashishk@gmail.com>
2023-06-19 23:40:25 +0530

    working on navbar

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="Welcome">
+        <h1>Welcome to My Plantation</h1>
+      </div>
+      <div className="navContainer">
+        <div className="lword">
+          <h1>Reforestation</h1>
+        </div>
+        <div className="nav-item">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>contact</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
